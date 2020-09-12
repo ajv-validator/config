c53fa4050265ac712b160969de685c233e0bfa2d
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2020-09-12 14:07:47 +0100

    eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
+const jsConfig = require("./.eslintrc_js")
+
 module.exports = {
-  env: { node: true },
-  root: true,
+  env: {node: true},
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: ["./tsconfig.json"],
@@ -12,4 +13,29 @@ module.exports = {
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier/@typescript-eslint",
   ],
-};
+  rules: {
+    ...jsConfig.rules,
+    "@typescript-eslint/adjacent-overload-signatures": "error",
+    "@typescript-eslint/array-type": "error",
+    "@typescript-eslint/ban-ts-comment": "error",
+    "@typescript-eslint/consistent-type-assertions": "error",
+    "@typescript-eslint/consistent-type-definitions": ["error"],
+    "@typescript-eslint/default-param-last": "error",
+    "dot-notation": "off",
+    "@typescript-eslint/dot-notation": "error",
+    "@typescript-eslint/explicit-function-return-type": ["error", {allowExpressions: true}],
+    "@typescript-eslint/explicit-member-accessibility": ["error", {accessibility: "no-public"}],
+    "@typescript-eslint/no-extraneous-class": "error",
+    "@typescript-eslint/no-misused-new": "error",
+    "@typescript-eslint/no-parameter-properties": "error",
+    "@typescript-eslint/no-unused-expressions": "error",
+    "@typescript-eslint/no-unused-vars": ["error", {argsIgnorePattern: "^_"}],
+    "@typescript-eslint/prefer-for-of": "error",
+    "@typescript-eslint/prefer-function-type": "error",
+    "@typescript-eslint/prefer-namespace-keyword": "error",
+    "@typescript-eslint/prefer-readonly": "error",
+    "@typescript-eslint/triple-slash-reference": "error",
+    "@typescript-eslint/unified-signatures": "error",
+    "@typescript-eslint/no-unnecessary-condition": ["error", {allowConstantLoopConditions: true}],
+  },
+}
